98374ff96dac5ec215f5470bfe6ddabd81a90399
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-04-05 03:22:28 -0400

    Logical steps to build the game.js

---
+++ assets/js/play-game.js
@@ -0,0 +1,21 @@
+//01 - target all the classes with querySelector.
+        //Discuss with Anthony and Maria, how to set up classes and ids.
+
+//class1 progressBar
+//Class2 question - get from API https://opentdb.com/
+//Class3 MultipleChoices
+//Class4 Score
+
+//02 Make a function that will retrive information from the API server
+    //add eventListner  on DOMcontentLoad - to html load first
+
+//03 make a function that will parse the API information and display as a question
+    //add eventListner on click to identify the answer
+
+//04 function to select the answer
+
+//05 input the results into a array and stringfy into localstorage.
+
+//06 function to cont the score
+
+//07 rediract to score/end page.
