be1faaca04609de440fbc9ccd6cd179138b60044
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-04-08 21:49:08 -0400

    Maria Test

--- assets/js/play-game.js
+++ assets/js/play-game.js
@@ -22,7 +22,23 @@ const newObject = {
 questiontest.push(newObject);
   
 
+if (savedPosts) {
+    const posts = JSON.parse(savedPosts);
 
+    // ForEach method used to create the div element throughout createElement
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+        // Populate the HTML element with post data
+        postElement.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.content}</p>
+            <p><b>By:</b> ${post.username}</p>
+            <p>${new Date().toLocaleString()}</p><br><hr>`; // I decided to add time for the post
+        // Append the post element to the recent posts container div element
+        recentPostsContainer.appendChild(postElement);
+    });
+}
 
 
 
