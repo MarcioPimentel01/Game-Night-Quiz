93ed1fa26731f6266dd3c29941f2c92a0b4e93b6
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-04-12 01:04:05 -0400

    Moving images to Folder
    Move to right Folder
    Fix Conflict into category.html
    HTML files created to sand box code

--- assets/js/tv.js
+++ assets/js/tv.js
@@ -1,27 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+    const classApiQuestion = document.querySelector('.api-question');
+    const classApiAnswer = document.querySelector('.api-answer');
 
-// document.addEventListener('DOMContentLoaded'), () => {
-
-async function loadQuestionTv() {
-    const APIUrlTv = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
-    const resultTv = await fetch(APIUrlTv);
-    const dataTv = await resultTv.json();
-    // console.log(dataFilms.resultFilms)
-    return dataFilms.results
+    async function loadQuestionTv() {
+        const APIUrlTv = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
+        const resultTv = await fetch(`${APIUrlTv}`);
+        const dataTv = await resultTv.json();
+        retrieveQuiz(dataTv.results[0]); // Call retrieveQuiz after fetching data
     }
 
-    function retrievequiz(dataTv){ 
+    function retrieveQuiz(dataTv){ 
         let correctAnswer = dataTv.correct_answer;
-        let incorrectAnswer = dataTv.incorrect_answer;
+        let incorrectAnswer = dataTv.incorrect_answers;
         let optionsList = [...incorrectAnswer];
-        optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-        optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, incorrectAnswer);
-        console.log(optionsList);
-        console.log(correctAnswer);
+        optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); //Fisher_yates algorithm
+
+        console.log(optionsList); // Displays shuffled answers, including the right one
+        console.log(correctAnswer); // Display the right one.
     }
 
-
-loadQuestionFilms() // I need to do something with the results, otherwiese the function will be calling non-stop
-retrievequiz(dataFilms)
-
-// }
+    loadQuestionTv(); // Call loadQuestionTv to start the process
+});
