7c6de407a941af60c5c8952904c4f3b6ab109c2f
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-04-11 21:09:54 -0400

    Update tv.js
    Working on TV.js

--- assets/js/tv.js
+++ assets/js/tv.js
@@ -2,19 +2,20 @@
 
 // document.addEventListener('DOMContentLoaded'), () => {
 
-async function loadQuestionFilms() {
-    const APIUrlFilms = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
-    const resultFilms = await fetch(APIUrlFilms);
-    const dataFilms = await resultFilms.json();
+async function loadQuestionTv() {
+    const APIUrlTv = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
+    const resultTv = await fetch(APIUrlTv);
+    const dataTv = await resultTv.json();
     // console.log(dataFilms.resultFilms)
     return dataFilms.results
     }
 
-    function retrievequiz(dataFilms){ 
-        let correctAnswer = dataFilms.correct_answer;
-        let incorrectAnswer = dataFilms.incorrect_answer;
+    function retrievequiz(dataTv){ 
+        let correctAnswer = dataTv.correct_answer;
+        let incorrectAnswer = dataTv.incorrect_answer;
         let optionsList = [...incorrectAnswer];
         optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+        optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, incorrectAnswer);
         console.log(optionsList);
         console.log(correctAnswer);
     }
