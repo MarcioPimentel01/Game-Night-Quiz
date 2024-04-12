55235ebd306413bfe19eb4b05a1976815e6a3d8c
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-04-11 20:34:54 -0400

    Js new files
    new files for Edwin and Juan

---
+++ assets/js/tv.js
@@ -0,0 +1,26 @@
+
+
+// document.addEventListener('DOMContentLoaded'), () => {
+
+async function loadQuestionFilms() {
+    const APIUrlFilms = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
+    const resultFilms = await fetch(APIUrlFilms);
+    const dataFilms = await resultFilms.json();
+    // console.log(dataFilms.resultFilms)
+    return dataFilms.results
+    }
+
+    function retrievequiz(dataFilms){ 
+        let correctAnswer = dataFilms.correct_answer;
+        let incorrectAnswer = dataFilms.incorrect_answer;
+        let optionsList = [...incorrectAnswer];
+        optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+        console.log(optionsList);
+        console.log(correctAnswer);
+    }
+
+
+loadQuestionFilms() // I need to do something with the results, otherwiese the function will be calling non-stop
+retrievequiz(dataFilms)
+
+// }
