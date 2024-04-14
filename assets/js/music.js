ac92e425b05d6c5274f22d44886e8147f1a51cd0
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-04-14 11:35:37 -0400

    music,js
    working on code to get fetch api and display question

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,5 +1,13 @@
-const question = document.getElementById("api-question");
-const choices = Array.from(document.getElementsByClassName("api-answer"));
+//Waiting for DOM content to load 
+document.addEventListener('DOMContentLoaded', () => {
+    //Selecting elements by class name
+    const [classApiQuestion, classApiAnswer] = document.querySelectorAll('.api-question, .api-answer');
+
+//Addindg event listener to selected answer
+classApiAnswer.addEventListener('click', (event) => {
+    //Check if element clickded is an answert option  
+})
+})
 
 let currentQuestion = {};
  let acceptingAnswers = false; //makes user waitin until everything on page load
@@ -8,22 +16,54 @@ let currentQuestion = {};
 
 let questions = [];
 
-fetch("https://opentdb.com/api.php?amount=10&category=12")
+fetch("https:opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple")
 .then(res => {
     return res.json();
 })
-.then(loadQuestionMusic => {
-    console.log(loadQuestionMusic.results);
-    loadQuestionMusic.results.map( loadQuestionMusic => {
+.then(loadedQuestion => {
+    console.log(loadedQuestion.results);
+    loadedQuestion.results.map(loadedQuestion => {
         const formattedQuestion = {
-            question : loadQuestionMusic.question
-        };        
-    })
+            question: loadedQuestion.questions
+        };
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
 
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index+1)] = choice;
+        })
+        return formattedQuestion;
+    });
 })
 .catch(err => {
     console.error(err);
 });
+
+
+async function loadQuestionMusic () {
+    const APIUrlMusic = 'https://opentdb.com/api.php?amount=12';
+    const resultMusic = await fetch(`${APIUrlMusic}`);
+    const dataMusic = resultMusic.json();
+    ArrayMusic.push(dataMusic);
+    console.log(dataMusic)
+}
+
+// function retrievequiz(dataMusic){ 
+
+//     let correctAnswer = dataMusic.correct_answer;
+//     let incorrectAnswer = dataMusic.incorrect_answer;
+//     let optionsList = [...incorrectAnswer];
+//     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+    
+//     console.log(optionsList);
+//     console.log(correctAnswer);
+// }
+    
+
+// .catch(err => {
+//     console.error(err);
+// });
 
 
 
@@ -40,19 +80,6 @@ fetch("https://opentdb.com/api.php?amount=10&category=12")
 
 
 
-
-// //Waiting for DOM content to load 
-// document.addEventListener('DOMContentLoaded', () => {
-//     //Selecting elements by class name
-//     const [classApiQuestion, classApiAnswer] = document.querySelectorAll('.api-question, .api-answer');
-
-//Addindg event listener to selected answer
-classApiAnswer.addEventListener('click', (event) => {
-    //Check if element clickded is an answert option
-    
-})
-
-})
 
 
 
